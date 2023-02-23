74df898e9285f035d5d81d5b57aa052252d97220
Momo Yang <momo.yang666@gmail.com>
2023-02-23 12:58:06 +0100

    changes

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
+
+
